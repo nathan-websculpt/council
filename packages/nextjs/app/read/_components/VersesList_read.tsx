475f3e1fe197978bf89c4b02c631ccc1cc0ecf52
nathan-websculpt <websculpt.nathan@gmail.com>
2024-06-26 09:11:34 -0500

    read page mobile changes to DDLs

--- packages/nextjs/app/read/_components/VersesList_read.tsx
+++ packages/nextjs/app/read/_components/VersesList_read.tsx
@@ -162,12 +162,16 @@ export const VersesList_Read = () => {
               <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
             </svg>
           </label>
-          <p className="text-xl">{viewStyleDisplayString}</p>
+          <p className="text-sm sm:text-md lg:text-xl">{viewStyleDisplayString}</p>
         </div>
-        <div className="flex flex-col justify-center gap-1 mt-4 md:justify-around md:flex-row lg:mt-0">
+        <div className="flex flex-row justify-center mt-4 sm:justify-around lg:mt-0">
           {metaData !== undefined && metaData !== null && (
             <>
-              <select className="px-6 py-2 mr-2 bg-primary" value={selectedChapter} onChange={changeChapter}>
+              <select
+                className="px-2 py-2 mr-1 text-xs sm:px-6 sm:py-2 sm:mr-2 bg-primary sm:text-sm md:text-md lg:text-lg"
+                value={selectedChapter}
+                onChange={changeChapter}
+              >
                 <option>{defaultChapterValue}</option>
                 {metaData.map(md => (
                   <option key={md.ChapterNumber.toString() + md.Verses.length.toString()} value={md.ChapterNumber}>
@@ -176,7 +180,11 @@ export const VersesList_Read = () => {
                 ))}
               </select>
 
-              <select className="px-6 py-2 mr-4 bg-primary" value={selectedVerse} onChange={changeVerse}>
+              <select
+                className="px-2 py-2 mr-1 text-xs sm:px-6 sm:py-2 sm:mr-4 bg-primary sm:text-sm md:text-md lg:text-lg"
+                value={selectedVerse}
+                onChange={changeVerse}
+              >
                 <option>{defaultVerseValue}</option>
                 {versesList !== undefined && versesList !== null && (
                   <>
@@ -190,7 +198,7 @@ export const VersesList_Read = () => {
               </select>
             </>
           )}
-          <button className="px-8 py-2 text-xl bg-primary" onClick={() => getClicked()}>
+          <button className="px-2 py-2 mr-1 text-xs sm:px-6 sm:py-2 sm:mr-2 sm:px-8 bg-primary sm:text-sm md:text-md lg:text-lg" onClick={() => getClicked()}>
             GET
           </button>
         </div>
