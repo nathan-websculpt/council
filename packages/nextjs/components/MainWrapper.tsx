65e594a9e83b6721a23705e05d4d390df02067ee
nathan-websculpt <websculpt.nathan@gmail.com>
2024-06-28 10:11:54 -0500

    tailwind comment to always remember

--- packages/nextjs/components/MainWrapper.tsx
+++ packages/nextjs/components/MainWrapper.tsx
@@ -5,6 +5,14 @@ interface Comp {
 export const MainWrapper = (_this: Comp) => {
   return (
     // text size: text-sm sm:text-md xl:text-xl
+    //
+    // TAILWIND NOTE _>
+    // none : regular phone
+    // sm   : big phone/small tablet
+    // md   : tablet (portrait)
+    // lg   : tablet (landscape)
+    // xl   : laptop/desktop
+    // 2xl  : bigger screens
     <>
       <div className="flex items-center justify-center">
         <div className="flex flex-col w-full px-1 py-2 md:py-6 md:px-6 lg:py-10 lg:px-8 xl:px-0 xl:py-12 lg:w-11/12 xl:w-3/5 bg-base-200 xl:border xl:border-base-300 xl:rounded-lg xl:shadow-xl ">
